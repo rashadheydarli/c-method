a6ee7a4b0d498b70eb06aa67728ddfe3354690e5
rashadheydarli <rashadahe@code.edu.az>
2023-01-22 19:27:43 +0400

    method added

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿
+
+// -----  Question 2-------
+
+//using System;
+//namespace method
+//{
+//    public class Program
+//    {
+//        static void Main()
+//        {
+//            int n = Convert.ToInt32(Console.ReadLine());
+//            Console.WriteLine(SumOfNumbers(n));
+//        }
+
+//        static int SumOfNumbers(int n)
+//        {
+//            int sum = 0;
+
+//            for (int i = 1; i < n; i++)
+//            {
+//                sum += i;
+//            }
+//            return sum;
+//        }
+//    }
+//}
+
+
+// ---------- Question 3 ---------------
+
+//using System;
+//namespace compare
+//{
+//    public class Program
+//    {
+//        static void Main()
+//        {
+//            Console.WriteLine("first");
+//            string n = Console.ReadLine();
+//            Console.WriteLine("second");
+//            string m = Console.ReadLine();
+//            Console.WriteLine(CompareString(n, m));
+//        }
+
+//        static string CompareString(string n, string m)
+//        {
+//            if (n.Length == m.Length)
+//            {
+//                return "beraberdir";
+//            }
+//            else if (n.Length > m.Length)
+//            {
+//                return "first is longer than second variable";
+//            }
+//            else
+//            {
+//                return "second  is longer than first variable ";
+//            }
+
+//        }
+//    }
+//}
+
+
+
